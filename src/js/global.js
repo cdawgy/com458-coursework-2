ab1b5dc96406a63287be5ff1f7cc932f1849185b
Cdawgy <cdawgy@Cdawgys-MacBook-Pro.local>
2022-03-14 22:58:23 +0000

    Initital commit with foundation code - includes all required files + bootstrap set up

---
+++ src/js/global.js
@@ -0,0 +1,5 @@
+// This is required to toggle on popovers on every page.
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
