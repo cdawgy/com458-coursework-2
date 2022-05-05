ff61a538c6779423c4fc07225a15f4ec100c1300
Cdawgy <cdawgy@Cdawgys-MacBook-Pro.local>
2022-05-05 20:18:41 +0100

    Refine modal can display and hide one mobile view

--- src/js/search-results/RefineSearchModalManager.js
+++
@@ -1,4 +0,0 @@
-export function setRefineSearchModalDisplayState(displayState) {
-  const modal = document.getElementById("refineSearchModal");
-  modal.style.display = displayState;
-}
--- src/js/utils/constants.js
+++ src/js/utils/constants.js
@@ -1,3 +1,5 @@
 export const QUERY_PARAMS_KEY = "queryParamsKey";
 export const SEARCH_PARAMS_KEY = "searchParamsKey";
 export const CLICKED_LISTING_KEY = "clickedListingKey";
+export const REFINE_SEARCH_MODAL_ID = "refineSearchModal";
+export const DARK_BG_ID = "dark-bg";
